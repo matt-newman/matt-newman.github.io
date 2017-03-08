6a5f2e727e8f87e228cfcdcb9d9d54a4ae3b3d2a
Matt Newman <matthew.newman@telegraph.co.uk>
2017-03-08 09:52:06 +0000

    Removing runnable js from main.js, making .gitignore extensive, fixing mailto: link.

---
+++ src/js/_mn/experiments.js
@@ -0,0 +1,23 @@
+var _mn = _mn || {};
+
+// _mn.log = function(text) {
+//     console.log('_mn.log:', text);
+// };
+//
+// var interval;
+//
+// (function(dom, utils) {
+//     var scroll = function() {
+//         console.log('scroll');
+//     };
+//
+//     dom.ready(function() {
+//         _mn.log('test');
+//         var debounced = utils.debounce(scroll, 100);
+//         debounced();
+//         interval = setInterval(function () {
+//             debounced();
+//         }, 40);
+//     });
+//
+// }(_mn.dom, _mn.utils));
