3f3068e8329df124216ad3f4ec6512c281848af1
Matt Newman <git@mattnewman.me>
2019-01-15 01:39:36 +0000

    updating grunt-contrib-sass to grunt-sass.

--- gruntfile.js
+++ gruntfile.js
@@ -126,7 +126,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-postcss');
